6ab97304e3632b92700ae98dd910cc59164b2a26
= <=>
2022-12-03 16:17:17 +0900

    12/3

---
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -0,0 +1,24 @@
+package com.shop.dto;
+
+import com.shop.entity.ItemImg;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@Getter @Setter
+public class ItemImgDto {
+
+    private Long id;
+
+    private String imgName; //이미지 파일명
+
+    private String oriImgName; //원본 이미지 파일
+
+    private String repimgYn; //대표 이미지 여부
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public static ItemImgDto of(ItemImg itemImg) {
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    }
+}
